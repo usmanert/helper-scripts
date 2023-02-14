397ddb5451aea2eb1a881833a693d259e9c98f64
abbasidd <usman.bsit122@iiu.edu.pk>
2023-02-14 15:25:06 +0500

    add seth estimate of poke

--- pricing/pokeOSM.sh
+++ pricing/pokeOSM.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-# import all signers account using ethsign import and then run the script 
-# ./setOSMPrice $PIP_ETH 
+# import all signers account using ethsign import and then run the script
+# ./pokeOSM.sh $PIP_ETH .env
 # $1 = PIP contract address
-# $ETH_FROM = sender
+# $2 envfile with ETH_FROM, etc.
+#$ETH_FROM = sender
 
+set -e
+set -u # or set -o nounset
+
+set -a
+source /data/helper-scripts/$2 # e.g. ethvar.bash
+
+tx=$(set -x; seth estimate --async "$1" 'poke()')
 
 echo "Sending PIP Poke tx..."
 
